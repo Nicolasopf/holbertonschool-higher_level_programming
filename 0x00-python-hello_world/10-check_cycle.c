10613f442f2e3b01daf62c39be51666c635ff011
Nicolas <nico15935746@gmail.com>
2020-12-07 13:17:18 -0500

    Another way to do it

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,16 +8,16 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *s1 = NULL, *s2 = NULL;
+	listint_t *s1, *s2;
 
-	s1 = s2 = list;
-	while (list && s1 && s2 && s1->next && s2->next)
+	if (!list)
+		return (0);
+	s1 = list, s2 = list;
+
+	while (s1 && s2 && s2->next)
 	{
-		s1 = s1->next;
-		s2 = s2->next->next;
-		if (!s2 || !s1)
-			return (0);
-		if (s2->next == s1)
+		s1 = s1->next, s2 = s2->next->next;
+		if (s1 == s2)
 			return (1);
 	}
 	return (0);
