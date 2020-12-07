9c85a8af40e8ccbc5aa995e4b89360275d417adf
Nicolas <nico15935746@gmail.com>
2020-12-07 13:13:10 -0500

    Changed var names

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,16 +8,16 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *f1 = NULL, *f2 = NULL;
+	listint_t *s1 = NULL, *s2 = NULL;
 
-	f1 = f2 = list;
-	while (list && f1 && f2 && f1->next && f2->next)
+	s1 = s2 = list;
+	while (list && s1 && s2 && s1->next && s2->next)
 	{
-		f1 = f1->next;
-		f2 = f2->next->next;
-		if (!f2 || !f1)
+		s1 = s1->next;
+		s2 = s2->next->next;
+		if (!s2 || !s1)
 			return (0);
-		if (f2->next == f1)
+		if (s2->next == s1)
 			return (1);
 	}
 	return (0);
