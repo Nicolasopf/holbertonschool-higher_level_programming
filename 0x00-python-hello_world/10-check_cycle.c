30e7103623ddc307e68cfa13e54703be886fce45
Nicolas <nico15935746@gmail.com>
2020-12-07 13:09:39 -0500

    Updated vart

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,13 +11,13 @@ int check_cycle(listint_t *list)
 	listint_t *f1 = NULL, *f2 = NULL;
 
 	f1 = list, f2 = list;
-	while (list && s1 && s2 && s1->next && s2->next)
+	while (list && f1 && f2 && f1->next && f2->next)
 	{
-		s1 = s1->next;
-		s2 = s2->next->next;
-		if (!s2 || !s1)
+		f1 = f1->next;
+		f2 = f2->next->next;
+		if (!f2 || !f1)
 			return (0);
-		if (s2->next == s1)
+		if (f2->next == f1)
 			return (1);
 	}
 	return (0);
