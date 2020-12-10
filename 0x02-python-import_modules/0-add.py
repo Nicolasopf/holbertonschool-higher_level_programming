af9a3414f539d443de99be6abf55ca699c4ece1b
Nicolas <nico15935746@gmail.com>
2020-12-10 13:29:53 -0500

    Import after if

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-import add_0 as asd
 
 if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, asd.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
