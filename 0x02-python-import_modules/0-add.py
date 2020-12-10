f827d83d1c76829c989eab2d40d2b5afe576d6c7
Nicolas <nico15935746@gmail.com>
2020-12-10 01:53:31 -0500

    Add something

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -5,4 +5,4 @@ import add_0 as asd
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, asd.add(a,b)))
+    print("{:d} + {:d} = {:d}".format(a, b, asd.add(a, b)))
