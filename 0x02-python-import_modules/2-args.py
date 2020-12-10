9fef234c67944b023290f156335b11ab31e1908a
Nicolas <nico15935746@gmail.com>
2020-12-10 13:37:21 -0500

    only

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
     from sys import argv
     h = 0
     leen = len(argv)
-    if leen >= 2:
+    if leen == 2:
+        print("1 argument:\n{:d}: {:s}".format(leen-1, argv[1]))
+    elif leen > 2:
         args = argv[1:]
         print(leen-1, "arguments:")
         for i in argv:
