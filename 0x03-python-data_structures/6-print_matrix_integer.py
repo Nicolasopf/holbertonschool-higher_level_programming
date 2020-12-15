27bc419bbf19b5db0756b1d362e7a5c2bac597d7
Nicolas <nico15935746@gmail.com>
2020-12-15 15:14:43 -0500

    Edge case added

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,8 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=' ')
-        print()
+    if matrix:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end=' ')
+            print()
