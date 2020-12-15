9fd0ca28f1fe21b4af7252b53d3a00310e59912d
Nicolas <nico15935746@gmail.com>
2020-12-15 15:20:29 -0500

    Return quited

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,6 +3,6 @@
 
 def print_reversed_list_integer(my_list=[]):
         if my_list:
-                return my_list.reverse()
+                my_list.reverse()
                 for i in my_list:
                         print("{:d}".format(i))
