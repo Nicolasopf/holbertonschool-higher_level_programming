40cd6dedc4e3a01d247a4a833bfcc93d8be237ae
Nicolas <nico15935746@gmail.com>
2020-12-15 15:26:38 -0500

    Spaces fixed

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,8 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in range(len(matrix)):
+            delim = ""
             for j in range(len(matrix[i])):
-                print("{:d}".format(matrix[i][j]), end=' ')
+                print("{:s}{:d}".format(delim, matrix[i][j]), end='')
+                delim = " "
             print()
