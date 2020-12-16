74c723a98d8c3bdcebee4bf2a1499a16ffc851a3
Nicolas <nico15935746@gmail.com>
2020-12-15 19:24:51 -0500

    Using temporary

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,9 +3,8 @@
 
 def max_integer(my_list=[]):
     if my_list:
-        a = 0
+        a = my_list[0]
         for i in my_list:
             if i > a:
                 a = i
         return a
-    return None
