a3629b1420217e4ec8f329a8ad80e3a141925b21
Nicolas <nico15935746@gmail.com>
2020-12-17 14:03:13 -0500

    Copies the list in another var

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    copy = my_list[:]
+    copy = my_list.copy()
     for i in range(len(copy)):
         if copy[i] == search:
             copy[i] == replace
