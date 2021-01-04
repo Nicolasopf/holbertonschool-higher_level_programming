c2930b93b0a3ebfcc5e434df4ac9945d6cf48f76
Nicolas <nico15935746@gmail.com>
2021-01-04 02:06:00 -0500

    No additional var

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,11 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    j = 0
     for i in range(x):
         try:
             print(my_list[i], end='')
-            j += 1
         except IndexError:
-            break
+            print()
+            return i
     print()
-    return j
+    return i + 1
