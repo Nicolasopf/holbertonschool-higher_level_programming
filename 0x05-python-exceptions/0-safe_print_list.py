7a96d2155031904d17df199c1997bbd0c29f39c0
Nicolas <nico15935746@gmail.com>
2021-01-04 13:53:13 -0500

    Return now J and not I

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,12 @@
 
 
 def safe_print_list(my_list=[], x=0):
+    j = 0
     for i in range(x):
         try:
             print(my_list[i], end='')
+            j += 1
         except IndexError:
-            print()
-            return i
+            break
     print()
-    return i + 1
+    return j
