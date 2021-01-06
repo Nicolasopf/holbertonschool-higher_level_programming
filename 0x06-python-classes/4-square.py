6b1bb2720b7f2d238368b7805fbc12695b548e9f
Nicolas <nico15935746@gmail.com>
2021-01-05 19:07:26 -0500

    Using get and set

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" Class with private instance size """
+
+
+class Square:
+    """ Check if size is less than 0, if it is an int and return area """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
