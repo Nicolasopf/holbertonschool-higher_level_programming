bee5651f7e3cada3509d314b414b177f66f6e2cc
Nicolas <nico15935746@gmail.com>
2021-01-15 13:54:23 -0500

    Is this fix?

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,7 +8,7 @@ def matrix_divided(matrix, div):
     if type(div) not in (int, float):
         raise TypeError("div must be a number")
 
-    if type(matrix) is not list or len(matrix) == 0:
+    if type(matrix) is not list or len(matrix) is 0:
         raise TypeError("matrix must be a matrix (list of lists) of integers/\
 floats")
 
@@ -16,14 +16,14 @@ floats")
         raise ZeroDivisionError("division by zero")
     tmp = [row[:] for row in matrix]
     for i in range(len(matrix)):
-        if type(matrix[i]) is not list or len(matrix[i]) == 0:
+        if type(matrix[i]) is not list or len(matrix[i]) is 0:
             raise TypeError("matrix must be a matrix (list of lists) of\
  integers/floats")
         if len(matrix[0]) != len(matrix[i]):
             raise TypeError("Each row of the matrix must have the same size")
-        for h in range(len(matrix[i])):
-            if type(tmp[i][h]) not in (int, float):
+        for h in matrix[i]:
+            if type(h) not in (int, float):
                 raise TypeError("matrix must be a matrix (list of lists) of\
  integers/floats")
-            tmp[i][h] = round(tmp[i][h] / div, 2)
-    return tmp
+
+    return [list(map(lambda n: round((n / div), 2), sub)) for sub in matrix]
