43742d86830c28069fb8a5986ea7eb22d63a96da
Nicolas <nico15935746@gmail.com>
2021-01-15 13:43:25 -0500

    Simple fix

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-""" Unittest for the function max_integer """
+"""Unittest for max_integer([..])
+"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    def test_maxint(self):
-        """ Check each list, numbers, combinations"""
-        self.assertEqual(max_integer([2, 2, 5]), 8)
-        self.assertEqual(max_integer([10, 10, 10]), 10)
-        self.assertEqual(max_integer([-41, -2, -1, 0, -95]), 0)
-        self.assertEqual(max_integer([2.8, 7.1, 8.8]), 4.7)
-        self.assertEqual(max_integer([76]), 76)
+        def test_maxint(self):
+            """Test different number lists"""
+            self.assertEqual(max_integer([15, 9, 6]), 15)
+            self.assertEqual(max_integer([10, 10, 10]), 10)
+            self.assertEqual(max_integer([-54, -7, 0, -84, -25]), 0)
+            self.assertEqual(max_integer([4.8, 3.2, 9.2]), 9.2)
+            self.assertEqual(max_integer([38]), 38)
 
-    def test_emptylist(self):
-        """Test void list"""
-        self.assertEqual(max_integer([]), None)
+        def test_emptylist(self):
+            """Test void list"""
+            self.assertEqual(max_integer([]), None)
 
-    def test_typeserror(self):
-        """Test errors values raised when neccesary"""
-        self.assertRaises(TypeError, max_integer, None)
-        self.assertRaises(TypeError, max_integer, (3 + 5j))
+        def test_typeserror(self):
+            """Test errors values raised when neccesary"""
+            self.assertRaises(TypeError, max_integer, None)
+            self.assertRaises(TypeError, max_integer, (3 + 5j))
