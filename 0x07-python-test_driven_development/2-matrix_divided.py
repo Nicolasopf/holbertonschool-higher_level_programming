508acda29abae6903409f77090e5185afab4f46d
Nicolas <nico15935746@gmail.com>
2021-01-14 20:46:35 -0500

    May this fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,16 +14,13 @@ floats")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
+    tmp = [row[:] for row in matrix]
     for i in range(len(matrix)):
         if type(matrix[i]) is not list or len(matrix[i]) == 0:
             raise TypeError("matrix must be a matrix (list of lists) of\
  integers/floats")
         if len(matrix[0]) != len(matrix[i]):
             raise TypeError("Each row of the matrix must have the same size")
-
-    tmp = [row[:] for row in matrix]
-    for i in range(len(matrix)):
         for h in range(len(matrix[i])):
             if type(tmp[i][h]) not in (int, float):
                 raise TypeError("matrix must be a matrix (list of lists) of\
