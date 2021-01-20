59128017bb7ff5bc44ae2ce4cde3e959a1b781cc
Nicolas <nico15935746@gmail.com>
2021-01-19 22:21:34 -0500

    Cass Square that inherits from Rectangle

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+""" Class Square that inherits from Rectangle (9-rectangle.py).\
+ (task based on 10-square.py). """
+
+
+class BaseGeometry:
+    """ you know it """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError(name + " must be an integer")
+        if value <= 0:
+            raise ValueError(name + " must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class inherite from BaseGeometry """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+
+
+class Square(Rectangle):
+    """ Square class inherite from Rectangle """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
