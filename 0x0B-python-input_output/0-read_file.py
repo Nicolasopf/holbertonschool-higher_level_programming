385c4d0afc3c983111fd5de7189feb11e7ab1d88
Nicolas <nico15935746@gmail.com>
2021-01-20 17:03:13 -0500

    Reads a text file using utf8

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Reads a text file (UTF8) and prints it to stdout """
+
+
+def read_file(filename=""):
+    """ with is more optimal than a loop """
+    with open(filename, encoding='UTF-8') as r:
+            print(reader.read(), end='')
