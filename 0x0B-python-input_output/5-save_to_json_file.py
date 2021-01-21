0a980425b58d7d55e095e5750077f0a27c40427e
Nicolas <nico15935746@gmail.com>
2021-01-21 15:23:45 -0500

    Don't return

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,8 +3,7 @@
 
 
 def save_to_json_file(my_obj, filename):
-    import json
-    json_rep = json.dumps(my_obj)
-
+    """ Same as above """
     with open(filename, encoding="UTF-8", mode="w+") as f:
-        return f.write(json_rep)
+        import json
+        f.write(json.dumps(my_obj))
