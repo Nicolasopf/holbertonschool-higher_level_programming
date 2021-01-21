60fa69243aa3ea27d7873118a1c41e59cc69ce30
Nicolas <nico15935746@gmail.com>
2021-01-21 16:33:25 -0500

    returns the dictionary description with simple data structure

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" returns the dictionary description with simple data structure """
+
+
+def class_to_json(obj):
+    """ You know this is not needed here """
+    return obj.__dict__
