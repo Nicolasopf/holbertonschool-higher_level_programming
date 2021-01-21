6500da19381d2c40f05f18b86ef3b353f4f37846
Nicolas <nico15935746@gmail.com>
2021-01-21 15:05:20 -0500

    Convert from string to json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" returns the JSON representation of an object (string) """
+
+
+def to_json_string(my_obj):
+    """ ABOCVEVEVE VE """
+    import json
+    return json.dumps(my_obj)
