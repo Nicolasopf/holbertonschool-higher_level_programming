421d3a1b800203f9b59575899e37dcaaf3f5aa1d
Nicolas <nico15935746@gmail.com>
2021-01-21 18:51:54 -0500

    defines a student (based on 9-student.py)

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Class Student that defines a student by """
+
+
+class Student:
+    """ Class Student that defines a student by:
+    Public instance attributes:
+    first_name
+    last_name
+    age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+
+        new_dict = {}
+        for i, j in self.__dict__.items():
+            if i in attrs:
+                new_dict[i] = j
+
+        return new_dict
