ca6a10c233abd0acba9e02371d73695819b320b9
Nicolas <nico15935746@gmail.com>
2021-01-21 19:30:35 -0500

    class Student that defines a student by (based on 10-student.py)

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Class Student that defines a student by """
+
+
+class Student:
+    """ Class Student that defines a student by:
+    Public instance attributes:
+    first_name
+    last_name
+    age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+
+        new_dict = {}
+        for i, j in self.__dict__.items():
+            if i in attrs:
+                new_dict[i] = j
+
+        return new_dict
+
+        def reload_from_json(self, json):
+            for key, value in json.items():
+                setattr(self, key, value)
