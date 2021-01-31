ded9eee8795215c7bf1775dd1d605824ad21bb68
Nicolas <nico15935746@gmail.com>
2021-01-30 20:53:08 -0500

    Comments added

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,23 +9,28 @@ class Square(Rectangle):
     I hope reach the goal.
     """
     def __init__(self, size, x=0, y=0, id=None):
+        """ Superman """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
+        """ Return the width """
         return self.width
 
     @size.setter
     def size(self, size):
+        """ Set the size of width and height """
         self.width = size
         self.height = size
 
     def __str__(self):
+        """ Prints the strings """
         i1 = '(' + str(self.id) + ') ' + str(self.x) + '/'
         i2 = str(self.y) + ' - ' + str(self.width)
         return "[Square] " + i1 + i2
 
     def update(self, *args, **kwargs):
+        """ Update the values """
         dic = ['id', 'size', 'x', 'y']
         if args and args[0] is not None:
             for i in range(len(args)):
@@ -35,4 +40,5 @@ class Square(Rectangle):
                 setattr(self, k, kwargs[k])
 
     def to_dictionary(self):
+        """ Return a dic """
         return {'id': self.id, 'size': self.width, 'x': self.x, 'y': self.y}
