e3bf9dc0a4f811b0aacf04841319382b57c6bcac
Nicolas <n.ico15935746@gmail.com>
2021-03-02 22:45:11 +0000

    Changed from int to integer

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -2,4 +2,4 @@
 --  creates the table id_not_null
 -- id INT with the default value 1
 -- name VARCHAR(256)
-CREATE TALBE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256))
+CREATE TALBE IF NOT EXISTS id_not_null(id INTEGER DEFAULT 1, name VARCHAR(256))
