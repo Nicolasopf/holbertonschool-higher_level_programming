6cdd07d6458e6b8fc25531f5f1504ed62d83a36c
Nicolas <n.ico15935746@gmail.com>
2021-03-02 22:44:34 +0000

    More comments added

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,3 +1,5 @@
 --  creates the table id_not_null
 --  creates the table id_not_null
+-- id INT with the default value 1
+-- name VARCHAR(256)
 CREATE TALBE IF NOT EXISTS id_not_null(id INT DEFAULT 1, name VARCHAR(256))
