3c98b43dacd7a99832751b0c5ca750621e45c465
Nicolas <n.ico15935746@gmail.com>
2021-03-02 22:52:16 +0000

    Added the database where to add the table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,4 +2,4 @@
 -- id int unique auto generated cant be null is primary key
 -- name varchar cant be null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL)
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
