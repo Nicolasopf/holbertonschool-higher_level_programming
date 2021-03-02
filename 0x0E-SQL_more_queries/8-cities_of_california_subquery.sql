e72ad566aacf2a6a1a1b46fe43e37770304f8fe0
Nicolas <n.ico15935746@gmail.com>
2021-03-02 23:26:48 +0000

    Show cities if name is california

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- list all cities of California
 -- listing now
-SELECT id, name FROM cities WHERE name = 'California' ORDER BY id
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id
