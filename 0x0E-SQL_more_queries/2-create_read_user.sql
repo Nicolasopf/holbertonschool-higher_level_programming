48cf1df18399334ec43b2b03bcb30b9a391f8c87
Nicolas <n.ico15935746@gmail.com>
2021-03-02 20:51:22 +0000

    CREATE DATABASE added

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- creates the database hbtn_0d_2 and the user user_0d_2
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
-CREATE IF NOT EXISTS hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2 . * TO 'user_od_2'@'localhost';
