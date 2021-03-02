15ab4e4cb01a7408c2188e4c6b83d5d0f059a3a0
Nicolas <n.ico15935746@gmail.com>
2021-03-02 22:51:29 +0000

    Creates a database, table, unique id auto generated and name not null

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- creates the database hbtn_0d_usa and the table states
+-- id int unique auto generated cant be null is primary key
+-- name varchar cant be null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL)
