39af406def38d3590d11b753278ac15cef9fc1fe
Nicolas <n.ico15935746@gmail.com>
2021-03-22 17:10:19 -0500

    [Updated] Style passed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 if (!process.argv[2] || !process.argv[3]) {
   console.log(0);
-
 } else {
   const args = (process.argv.splice(2, process.argv.length)).map((num) => parseInt(num));
   const max = Math.max(...args);
