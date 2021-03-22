59ffea58ed2cea43d17895590cdea8b82fcc9f63
root <n.ico15935746@gmail.com>
2021-03-22 19:30:31 +0000

    [Updated] Fixed semistandard tabs

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,8 @@
 const args = process.argv;
 const num = parseInt(args[2]);
 if (isNaN(num)) {
-    console.log('Missing number of occurrences');
-}
-else {
+  console.log('Missing number of occurrences');
+} else {
   for (let i = 0; i < num; i++) {
     console.log('C is fun');
   }
