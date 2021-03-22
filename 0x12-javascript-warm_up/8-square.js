cf77c0e3cd382329668aa49e4a277b21bdc2f9c6
Nicolas <n.ico15935746@gmail.com>
2021-03-22 14:34:52 -0500

    [Updated] Output now is 'Missing size'

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 const args = process.argv;
 const num = parseInt(args[2]);
 if (isNaN(num)) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < num; i++) {
     console.log('X'.repeat(num));
