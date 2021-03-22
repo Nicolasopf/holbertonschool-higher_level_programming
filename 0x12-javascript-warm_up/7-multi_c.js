d9fde549677122cf788d19b0411f4b5b98667438
Nicolas <n.ico15935746@gmail.com>
2021-03-22 03:09:39 -0500

    [Added] Prints x times “C is fun”

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv;
+const num = parseInt(args[2]);
+if (num === num) {
+    for (i = 0; i < num; i++) {
+	console.log('C is fun');
+    }
+} else {
+    console.log('Missing number of occurrences');
+}
