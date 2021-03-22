b599e6f6ca8dbea0dde16d9a5999b7851fab6acb
Nicolas <n.ico15935746@gmail.com>
2021-03-22 14:28:57 -0500

    [Updated] Using isNaN instead of comparing itself

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const args = process.argv;
 const num = parseInt(args[2]);
-if (num === num) {
+if (isNaN(num)) {
+    console.log('Missing number of occurrences');
+}
+else {
   for (let i = 0; i < num; i++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
