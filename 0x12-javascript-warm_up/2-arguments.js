0a174c0830f41710e4696a983611e4959274b17d
Nicolas <n.ico15935746@gmail.com>
2021-03-22 14:22:28 -0500

    [Updated] Changed output from Found to found

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,7 +3,7 @@ const args = process.argv;
 if (args.length < 3) {
   console.log('No argument');
 } else if (args.length === 3) {
-  console.log('Argument Found');
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
