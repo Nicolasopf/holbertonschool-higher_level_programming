e25cbc84dc5e6d6a86a01d309fa9baa11198fff0
Nicolas <n.ico15935746@gmail.com>
2021-03-23 05:39:54 -0500

    [Added] Prints rectangle with a char, if not defined, print it with X

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const Sqash = require('./5-square');
+
+class Square extends Sqash {
+    charPrint(c) {
+	if (typeof(c) === 'undefined') {
+	    c = 'X';
+	}
+	for (let i = 0; i < this.height; i++) {
+	    console.log(c.repeat(this.width));
+	}
+    }
+}
+
+module.exports = Square;
