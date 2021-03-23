660a44aea56964d2f391b617c48a3d56a1436c40
root <n.ico15935746@gmail.com>
2021-03-23 20:01:12 +0000

    [Updated] Semistandard fixed

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-  if (w > 0 || h > 0) {
-    this.width = w;
-    this.height = h;
-  }
+    if (w > 0 || h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 
   print () {
-  for (let i = 0; i < this.height; i++) {
-    console.log('X'.repeat(this.width));
-  }
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
   }
 }
 
