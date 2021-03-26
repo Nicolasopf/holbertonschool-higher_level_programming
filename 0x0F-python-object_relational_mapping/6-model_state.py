bdfb8b13061efb12140033c7a16bfd3f4ed03e33
Nicolas <n.ico15935746@gmail.com>
2021-03-26 19:45:26 +0000

    [Updated] This file was in the intranet output

--- 0x0F-python-object_relational_mapping/6-model_state.py
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-""" Connect to database """
-from sys import argv
+"""Start link class to table in database
+"""
+import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
 
-if __name__ != '__main__':
-    exit
+from sqlalchemy import (create_engine)
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}').format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3])
-Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
