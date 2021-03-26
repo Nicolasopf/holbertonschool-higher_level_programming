cad4fadc50b2d9017e51862ddd0683043de00c79
Nicolas <n.ico15935746@gmail.com>
2021-03-26 14:42:24 -0500

    [Added] Creates the engine

---
+++ 0x0F-python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Connect to database """
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+
+if __name__ != '__main__':
+    exit
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}').format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3])
+Base.metadata.create_all(engine)
