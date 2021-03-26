cbc88fd567fac4a1e7447e2030dd67d49c7575ef
Nicolas <n.ico15935746@gmail.com>
2021-03-26 20:03:14 +0000

    [Added] Lists all State objects from the database

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ != "__main__":
+    exit
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    argv[1], argv[2], argv[3]), pool_pre_ping=True)
+Session = sessionmaker()
+session = Session(bind=engine)
+
+query = session.query(State).order_by(State.id)
+
+for item in query:
+    print("{}: {}".format(item.id, item.name))
