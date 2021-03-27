45323794c1dc9701f4c8d7f0c45bdfb9f98fa986
Nicolas <n.ico15935746@gmail.com>
2021-03-27 07:07:03 +0000

    [Updated] All code moved to statement conditions

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,22 +4,20 @@
 from sys import argv
 import MySQLdb as mysql
 
-if __name__ != '__main__':
-    exit
+if __name__ == '__main__':
+    connection = mysql.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
 
-connection = mysql.connect(
-    host='localhost',
-    port=3306,
-    user=argv[1],
-    passwd=argv[2],
-    db=argv[3]
-)
+    cursor = connection.cursor()
 
-cursor = connection.cursor()
+    syntax = 'SELECT states.id, states.name FROM states WHERE\
+    states.name = \'{}\' ORDER BY states.id'.format(argv[4])
+    cursor.execute(syntax)
 
-syntax = 'SELECT states.id, states.name FROM states WHERE\
- states.name = \'{}\' ORDER BY states.id'.format(argv[4])
-cursor.execute(syntax)
-
-for column in cursor:
-    print(column)
+    for column in cursor:
+        print(column)
