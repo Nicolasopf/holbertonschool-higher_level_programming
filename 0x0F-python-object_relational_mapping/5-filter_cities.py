1561c1fea5c144a1f916d70bba4e48cb210ed0c3
Nicolas <n.ico15935746@gmail.com>
2021-03-27 08:00:19 +0000

    [Updated] Prints with a join

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -21,9 +21,5 @@ if __name__ == "__main__":
     ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id'
     cursor.execute(query, (state, ))
 
-    tmp_list = []
-    for column in cursor:
-        tmp_list.append(column[0])
-    for item in range(len(tmp_list) - 1):
-        print(tmp_list[item], end=", ")
-    print(tmp_list[item + 1])
+    result = [i[0] for i in cursor]
+    print(', '.join(result))
