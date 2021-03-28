443fe696d8423d78c03eb3d3ecc8375c6f2bec6e
Nicolas <n.ico15935746@gmail.com>
2021-03-28 18:55:21 +0000

    [Added] Same as model_city

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" class definition of a State and an instance Base = declarative_base() """
+from sqlalchemy import Column, Integer, String, MetaData, ForeignKey
+from relationship_state import Base
+
+
+class City(Base):
+    """ Link to mysql table states """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
