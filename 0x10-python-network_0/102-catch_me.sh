a68d51106b1eddcdafef3ab822d09de2a912805d
Nicolas <n.ico15935746@gmail.com>
2021-04-08 14:09:27 -0500

    [Added] Flag -s added

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes the server respond with a message "You got me!"
-curl -X PUT 0:5000/catch_me -L -d "user_id=98" -H "Origin: HolbertonSchool"
+curl -s -X PUT 0:5000/catch_me -L -d "user_id=98" -H "Origin: HolbertonSchool"
