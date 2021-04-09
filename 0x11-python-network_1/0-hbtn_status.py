25c0687e1664cd242612bd430e451963c93790b6
Nicolas <n.ico15935746@gmail.com>
2021-04-09 14:47:43 -0500

    [Added] Print the status using urllib

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Fetch intranet status """
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+          .format(type(html), html, html.decode("utf-8")))
