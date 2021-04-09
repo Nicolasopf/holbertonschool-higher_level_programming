7971f715475fc9f1bba87fe1bca4ea5b4896378c
Nicolas <n.ico15935746@gmail.com>
2021-04-09 15:37:03 -0500

    [Added] New conditionals implemented

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,10 +6,18 @@ from sys import argv
 
 if __name__ == "__main__":
     if len(argv) != 2:
-        print("No result")
-        exit()
+        q = ""
+    else:
+        q = argv[1]
     response = requests.post(
         "http://110bb9ddcf0f.b594c0bb.hbtn-cod.io:5000/search_user",
-        data={"q": argv[1]})
+        data={"q": q})
+
     text = response.json()
-    print("[{}] {}".format(text['id'], text['name']))
+    if str(text) == "{}":
+        print("No result")
+    else:
+        try:
+            print("[{}] {}".format(text['id'], text['name']))
+        except KeyError:
+            print("Not a valid JSON")
