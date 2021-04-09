161c7986e57e05f33173f09e58472ea263c60150
Nicolas <n.ico15935746@gmail.com>
2021-04-09 16:06:33 -0500

    [Added] Try and pass

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,8 +7,11 @@ from sys import argv
 if __name__ == "__main__":
     info = requests.get("https://api.github.com/repos/" + argv[1] + "/" +
                         argv[2] + "/commits")
-    text = info.json()
-    for dic in text[:10]:
-        sha = dic.get('sha')
-        commit = dic.get('commit')
-        print(sha + ": " + commit.get('author').get('name'))
+    try:
+        text = info.json()
+        for dic in text[:10]:
+            sha = dic.get('sha')
+            commit = dic.get('commit')
+            print(sha + ": " + commit.get('author').get('name'))
+    except:
+        pass
