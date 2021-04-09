9c0042c5f9068d03f0136fedac8e7167e7cbc276
Nicolas <n.ico15935746@gmail.com>
2021-04-09 16:10:56 -0500

    [Deleted] No try

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,14 +7,12 @@ from sys import argv
 if __name__ == "__main__":
     info = requests.get("https://api.github.com/repos/" + argv[1] + "/" +
                         argv[2] + "/commits")
-    try:
-        text = info.json()
-        for dic in text[:10]:
-            sha = dic.get('sha')
-            commit = dic.get('commit')
-            if commit.get('author'.get('name')):
-                print(sha + ": " + commit.get('author').get('name'))
-            else:
-                print(sha + ": None")
-    except:
-        pass
+
+    text = info.json()
+    for dic in text[:10]:
+        sha = dic.get('sha')
+        commit = dic.get('commit')
+        if commit.get('author').get('name'):
+            print(sha + ": " + commit.get('author').get('name'))
+        else:
+            print(sha + ": None")
