5da306c319766aceea19161115851eb5de063fae
Nicolas <n.ico15935746@gmail.com>
2021-05-04 14:40:46 -0500

    [Updated] Semistandard passed

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,8 +3,5 @@ const fs = require('fs');
 const argv = process.argv;
 
 fs.writeFile(argv[2], argv[3], 'utf8', function(err) {
-	if (err) {
-		console.log(err);
-	}
-	return;
+	if (err) throw err;
 });
