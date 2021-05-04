443f7c9f599f841c5cfe4abacfb75314483d3d64
Nicolas <n.ico15935746@gmail.com>
2021-05-04 14:42:27 -0500

    [Updated] Err changed to error

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,6 +2,6 @@
 const fs = require('fs');
 const argv = process.argv;
 
-fs.writeFile(argv[2], argv[3], 'utf8', function(err) {
-	if (err) throw error;
+fs.writeFile(argv[2], argv[3], 'utf8', function(error) {
+	if (error) throw error;
 });
