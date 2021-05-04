6c205dd0cc7f60ca1e9745b6d86b249e0bb38427
Nicolas <n.ico15935746@gmail.com>
2021-05-04 14:56:11 -0500

    [Updated] Semistandard passed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,7 +5,7 @@ const argv = process.argv;
 request('https://swapi-api.hbtn.io/api/films/' + argv[2], function (error, response) {
   if (error) {
     return console.error(error);
-}
+  }
   const jsonResp = JSON.parse(response.body);
   console.log(jsonResp.title);
 });
