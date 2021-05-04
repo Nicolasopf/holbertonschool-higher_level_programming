1259704421f524dd4613c37de40b9ad0483aa1cb
Nicolas <n.ico15935746@gmail.com>
2021-05-04 14:40:05 -0500

    [Updated] Semistandard passed

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const argv = process.argv;
-const request = require('request')
+const request = require('request');
 
 request(argv[2], function(err, response) {
 	if (err) {
-		console.log(err)
+		console.log(err);
 	}
 	else {
-		console.log("code: " + response.statusCode)
+		console.log("code: " + response.statusCode);
 	}
-	return
-})
+	return;
+});
