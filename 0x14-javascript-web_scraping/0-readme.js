251d16887fb53493c7fffac2d9b4a1260619f1d5
Nicolas <n.ico15935746@gmail.com>
2021-05-04 00:48:56 -0500

    [Added] Script to read a file passed as argument

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs')
+const argv = process.argv;
+
+fs.readFile(argv[2], 'utf8', (err, data) => {
+	if (err) {
+		console.log(err)
+		return
+	}
+	console.log(data)
+})
