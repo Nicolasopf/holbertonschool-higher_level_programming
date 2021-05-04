d8bb8cea8b7ceba9adcd2d15062f2742c4a62bd7
Nicolas <n.ico15935746@gmail.com>
2021-05-04 14:33:47 -0500

    [Updated] Semistandard passed

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@ const fs = require('fs');
 const argv = process.argv;
 
 fs.readFile(argv[2], 'utf8', (err, data) => {
-	if (err) {
-		console.log(err);
-		return;
-	}
-	console.log(data);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
 })
