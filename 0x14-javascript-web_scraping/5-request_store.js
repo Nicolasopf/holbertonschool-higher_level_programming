a1ea6224250757775cdc2922afe6c331ef1470b1
Nicolas <n.ico15935746@gmail.com>
2021-05-04 14:59:22 -0500

    [Updated] Semistandard passed

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,15 +3,13 @@ const argv = process.argv
 const request = require('request')
 const fs = require('fs')
 
-request(argv[2], 'utf8', function(err, response) {
-	if (err) {
-		console.log(err)
-		return
+request(argv[2], 'utf8', function(error, response) {
+  if (error) {
+    return console.error(error);
+  }
+  fs.writeFile(argv[3], response.body, 'utf-8', function(error) {
+	if (error) {
+   	  return console.error(error)
 	}
-	fs.writeFile(argv[3], response.body, 'utf-8', function(err) {
-		if (err) {
-			console.log(err)
-		}
-		return
-	})
-})
+  });
+});
