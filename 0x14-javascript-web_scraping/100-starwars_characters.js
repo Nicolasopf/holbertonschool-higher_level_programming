9bfacf45e61852d686979becc001c05d0ccd3250
Nicolas <n.ico15935746@gmail.com>
2021-05-04 21:12:13 -0500

    [Added] Semistandard passed

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,15 +2,14 @@
 const argv = process.argv;
 const request = require('request');
 
-request("https://swapi-api.hbtn.io/api/films/" + argv[2], 'utf8', function (error, response, body) {
+request('https://swapi-api.hbtn.io/api/films/' + argv[2], 'utf8', function (error, response, body) {
   if (error) throw (error);
-  const tasksDic = {};
   const result = JSON.parse(body);
-  const chars = result.characters
+  const chars = result.characters;
   chars.forEach(person => {
     request(person, 'utf8', function (error, response, body) {
-		console.log(JSON.parse(body).name)
-	});
+      if (error) throw error;
+      console.log(JSON.parse(body).name);
+    });
   });
-//  console.log(tasksDic);
 });
