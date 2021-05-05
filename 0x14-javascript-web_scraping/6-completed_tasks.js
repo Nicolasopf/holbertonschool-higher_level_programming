ef136d278a66550bccc9ba159e83ae182a80e782
Nicolas <n.ico15935746@gmail.com>
2021-05-04 19:38:58 -0500

    [Updated] Semistandard passed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,19 +2,19 @@
 const argv = process.argv;
 const request = require('request');
 
-request(argv[2], 'utf8', function(error, response, body) {
-  if (error) throw(error);
-  const tasks_dic = {}
+request(argv[2], 'utf8', function (error, response, body) {
+  if (error) throw (error);
+  const tasksDic = {};
   const result = JSON.parse(body);
 
   result.forEach(task => {
-    if (task.completed == true) {
-      if (tasks_dic[task.userId] === undefined) {
-		tasks_dic[task.userId] = 1;
-	  } else {
-		tasks_dic[task.userId]++;
-	  }
-	}
+    if (task.completed === true) {
+      if (tasksDic[task.userId] === undefined) {
+        tasksDic[task.userId] = 1;
+      } else {
+        tasksDic[task.userId]++;
+      }
+    }
   });
-  console.log(tasks_dic);
+  console.log(tasksDic);
 });
